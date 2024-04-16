3d6cfd6b9c22d39cc0fb328bd1833e08608d8cb6
anupam8nith <anupamkumar.nith@gmail.com>
2024-04-16 17:17:10 +0530

    [DEPLOYED]

--- app.py
+++ app.py
@@ -1,54 +1,53 @@
+import os
 from flask import Flask, redirect, render_template, request, url_for
-app = Flask(__name__)
-
 from flask_sqlalchemy import SQLAlchemy
 
-# /// = relative path , //// = absolute path
+app = Flask(__name__)
+
+# Configure the Flask app
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'dev'  # Add a secret key for session management
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.instance_path, 'instance.sqlite')
+
+# Initialize SQLAlchemy
 db = SQLAlchemy(app)
 
-
-# Model for the ToDo list -app 
+# Model for the ToDo list app
 class ToDo(db.Model):
-    id = db.Column(db.Integer, primary_key =True)
+    id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     complete = db.Column(db.Boolean)
 
-
+# Routes
 @app.route('/')
 def home():
     todo_list = ToDo.query.all()
     return render_template("base.html", todo_list=todo_list)
 
-#operations
-
-#add
-@app.route("/add", methods =["POST"])
+@app.route("/add", methods=["POST"])
 def add():
     title = request.form.get("title")
-    new_todo = ToDo(title=title,complete=False)
+    new_todo = ToDo(title=title, complete=False)
     db.session.add(new_todo)
     db.session.commit()
     return redirect(url_for("home"))
 
-#update
 @app.route("/update/<int:todo_id>")
 def update(todo_id):
-    todo = ToDo.query.filter_by(id=todo_id).first
+    todo = ToDo.query.get_or_404(todo_id)
     todo.complete = not todo.complete
     db.session.commit()
     return redirect(url_for("home"))
 
-#delete
 @app.route("/delete/<int:todo_id>")
 def delete(todo_id):
-    todo = ToDo.query.filter_by(id = todo_id).first
+    todo = ToDo.query.get_or_404(todo_id)
     db.session.delete(todo)
     db.session.commit()
     return redirect(url_for("home"))
 
-
-if __name__ =="__main__":
-    db.create_all()
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
